64a8f7987b8b7d6dc12beee9ae7c1a41e0ae380d
henriquebarkett <henrique.barkett02@gmail.com>
2025-03-19 09:48:28 -0300

    test: Testing mock http request

---
+++ src/infra/swapi_api_consumer_test.py
@@ -0,0 +1,10 @@
+from .swapi_api_consumer import SwapiApiConsumer
+
+def test_get_starships(requests_mock):
+
+    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
+
+    swapi_api_consumer = SwapiApiConsumer()
+    response = swapi_api_consumer.get_starships(page=4)
+
+    print(response)
